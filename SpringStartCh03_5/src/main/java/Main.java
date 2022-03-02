123411996e7b1bc23f572871c3a2460dd740540d
Anna Łuczak <an.archeo@gmail.com>
2022-03-02 22:50:52 +0000

    @Autowired by setter example

---
+++ SpringStartCh03_5/src/main/java/Main.java
@@ -0,0 +1,16 @@
+import beans.Person;
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Person person = context.getBean(Person.class);
+
+        System.out.println("Person's name: " + person.getName());
+
+        System.out.println("Person's parrot: " + person.getParrot());
+
+    }
+}
