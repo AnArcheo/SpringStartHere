47852982ff876e8b93198339a18dd0b3449863e8
Anna Łuczak <an.archeo@gmail.com>
2022-03-03 23:29:19 +0000

    created DBCommentRepository implementation of CommentRepository

---
+++ SpringStartCh04_1/src/main/java/repositories/DBCommentRepository.java
@@ -0,0 +1,10 @@
+package repositories;
+
+import model.Comment;
+
+public class DBCommentRepository implements CommentRepository{
+    @Override
+    public void storeComment(Comment comment) {
+        System.out.println("Storing comment: " + comment.getText());
+    }
+}
