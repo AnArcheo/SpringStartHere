3e5ca360f6a44331e08b20d7106063ea0a5926fa
Anna Łuczak <an.archeo@gmail.com>
2022-03-03 23:05:37 +0000

    updated Comment POJO

--- SpringStartCh04_1/src/main/java/model/Comments.java
+++ SpringStartCh04_1/src/main/java/model/Comments.java
@@ -5,4 +5,20 @@ public class Comments {
     private String author;
 
     private String text;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
