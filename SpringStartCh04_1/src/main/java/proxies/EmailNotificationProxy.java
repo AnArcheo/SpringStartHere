c52632d8d70120baccff3f06c6f28656f3346541
Anna Łuczak <an.archeo@gmail.com>
2022-03-03 23:27:56 +0000

    created EmailNotificationProxy implementation of CommentNotificationProxy

---
+++ SpringStartCh04_1/src/main/java/proxies/EmailNotificationProxy.java
@@ -0,0 +1,10 @@
+package proxies;
+
+import model.Comment;
+
+public class EmailNotificationProxy implements CommentNotificationProxy{
+    @Override
+    public void sendComment(Comment comment) {
+        System.out.println("Sending notification for comment: " + comment.getText());
+    }
+}
