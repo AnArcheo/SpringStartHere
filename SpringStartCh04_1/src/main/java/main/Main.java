a594d0d553dc72de59f8fd84bd92d0bc1a454618
Anna Łuczak <an.archeo@gmail.com>
2022-03-04 23:28:31 +0000

    created Main class

--- SpringStartCh04_1/src/main/java/main/Main.java
+++ SpringStartCh04_1/src/main/java/main/Main.java
@@ -1,4 +1,25 @@
 package main;
 
+import model.Comment;
+import proxies.EmailNotificationProxy;
+import repositories.CommentRepository;
+import repositories.DBCommentRepository;
+import services.CommentService;
+
 public class Main {
+    public static void main(String[] args) {
+        // instances for dependecies
+        var commentRepository = new DBCommentRepository();
+        var commentNotificationProxy = new EmailNotificationProxy();
+
+        // instance of the service class
+        var commentService = new CommentService(commentRepository, commentNotificationProxy);
+
+        // instance of comment to send as parameter to the publish comment use case
+        var comment = new Comment();
+        comment.setAuthor("Anna");
+        comment.setText("Test Comment");
+
+        commentService.publishComment(comment);
+    }
 }
