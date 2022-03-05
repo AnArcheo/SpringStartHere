01479f56793e16f5e05ceb667c8e9b673475536e
Anna Łuczak <an.archeo@gmail.com>
2022-03-05 23:34:07 +0000

    created CommentPushNotificationProxy class as a second implementation of CommentNotificationProxy interface

---
+++ SpringStartCh04_1/src/main/java/proxies/CommentPushNotificationProxy.java
@@ -0,0 +1,14 @@
+package proxies;
+
+import model.Comment;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary //this implementation as primary (more implementation may cause exception if Spring won't know which one to choose)
+public class CommentPushNotificationProxy implements CommentNotificationProxy{
+    @Override
+    public void sendComment(Comment comment) {
+        System.out.println("Sending push notification for comment: " + comment.getText());
+    }
+}
