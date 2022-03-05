3d83df3c9716c1dc75e3790ae04719a488922afc
Anna Łuczak <an.archeo@gmail.com>
2022-03-05 23:07:48 +0000

    updated Main class after spring DI

--- SpringStartCh04_1/src/main/java/main/Main.java
+++ SpringStartCh04_1/src/main/java/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 
+import config.ProjectConfiguration;
 import model.Comment;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import proxies.EmailNotificationProxy;
 import repositories.CommentRepository;
 import repositories.DBCommentRepository;
@@ -8,18 +10,27 @@ import services.CommentService;
 
 public class Main {
     public static void main(String[] args) {
-        // instances for dependecies
-        var commentRepository = new DBCommentRepository();
-        var commentNotificationProxy = new EmailNotificationProxy();
+        // how main class looks without spring framework:
+//        // instances for dependecies
+//        var commentRepository = new DBCommentRepository();
+//        var commentNotificationProxy = new EmailNotificationProxy();
+//
+//        // instance of the service class
+//        var commentService = new CommentService(commentRepository, commentNotificationProxy);
+//
+//        // instance of comment to send as parameter to the publish comment use case
+//        var comment = new Comment();
+//        comment.setAuthor("Anna");
+//        comment.setText("Test Comment");
+//
+//        commentService.publishComment(comment);
 
-        // instance of the service class
-        var commentService = new CommentService(commentRepository, commentNotificationProxy);
-
-        // instance of comment to send as parameter to the publish comment use case
+        //Main class after using Spring Dependency Injection:
+        var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
         var comment = new Comment();
         comment.setAuthor("Anna");
         comment.setText("Test Comment");
-
+        var commentService = context.getBean(CommentService.class);
         commentService.publishComment(comment);
     }
 }
