492454c5462a3461d030e4504f17f4670c329fdd
Anna Łuczak <an.archeo@gmail.com>
2022-03-07 23:16:36 +0000

    examples from chapter 5. @Lazy annotation

--- SpringStartCho05_1/src/main/java/main/Main.java
+++ SpringStartCho05_1/src/main/java/main/Main.java
@@ -2,9 +2,19 @@ package main;
 
 import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.CommentService;
 
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        System.out.println("before retrieving the CommentService");
+        var service = context.getBean(CommentService.class); // first reference to the bean
+        System.out.println("After retrieving the Comment Service");
+
+        //prints:
+//        before retrieving the CommentService
+//        Comment service created!
+//        After retrieving the Comment Service
     }
 }
--- SpringStartCho05_1/src/main/java/services/CommentService.java
+++ SpringStartCho05_1/src/main/java/services/CommentService.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
-@Lazy
+@Lazy // spring will create instance when reference to the bean will be used
 public class CommentService {
     public CommentService() {
         System.out.println("Comment service created!");
