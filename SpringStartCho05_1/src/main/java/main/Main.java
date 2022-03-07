5395ddcb7ea595d0566254997b73bc5a1d476d36
Anna Łuczak <an.archeo@gmail.com>
2022-03-07 23:11:48 +0000

    examples from chapter 5. @Lazy annotation

---
+++ SpringStartCho05_1/src/main/java/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+    }
+}
---
+++ SpringStartCho05_1/src/main/java/services/CommentService.java
@@ -0,0 +1,12 @@
+package services;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+@Service
+@Lazy
+public class CommentService {
+    public CommentService() {
+        System.out.println("Comment service created!");
+    }
+}
