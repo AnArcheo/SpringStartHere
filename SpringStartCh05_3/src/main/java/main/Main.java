4c3fe335bb4f62f7534a45bcf6070cb63ebb13ab
Anna Łuczak <an.archeo@gmail.com>
2022-03-08 22:32:09 +0000

    initial commit

--- SpringStartCh05_3/src/main/java/main/Main.java
+++ SpringStartCh05_3/src/main/java/main/Main.java
@@ -1,4 +1,18 @@
 package main;
 
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.CommentService;
+import services.UserService;
+
 public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        var s1 = context.getBean(CommentService.class);
+        var s2 = context.getBean(UserService.class);
+
+        boolean b = s1.getCommentRepository() == s2.getCommentRepository();
+        System.out.println(b);
+    }
 }
--- SpringStartCh05_3/src/main/java/services/CommentService.java
+++ SpringStartCh05_3/src/main/java/services/CommentService.java
@@ -1,4 +1,15 @@
 package services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repositories.CommentRepository;
+
+@Service
 public class CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+
+    public CommentRepository getCommentRepository(){
+        return commentRepository;
+    }
 }
