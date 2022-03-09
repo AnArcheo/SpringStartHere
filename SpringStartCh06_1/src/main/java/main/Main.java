5275910002dc9108c58fa294b69b87f4e5966ce1
Anna Łuczak <an.archeo@gmail.com>
2022-03-09 23:39:15 +0000

    created Main class

--- SpringStartCh06_1/src/main/java/main/Main.java
+++ SpringStartCh06_1/src/main/java/main/Main.java
@@ -1,4 +1,23 @@
 package main;
 
+import config.ProjectConfig;
+import model.Comment;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.CommentService;
+
 public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        //gets the CommentService from the context
+        var service = context.getBean(CommentService.class);
+
+        //creates a comment instance to give as a parameter to the publishComment() method
+        Comment comment = new Comment();
+        comment.setText("Demo Text");
+        comment.setAuthor("Anna");
+
+        //Calls the publishComment() method
+        service.publishComment(comment);
+    }
 }
