9854e46f09fb6f446512e02b0011f83e05ff0593
Anna Łuczak <an.archeo@gmail.com>
2022-03-10 23:15:07 +0000

    updated ProjectConfig class to include aspect

--- SpringStartCh06_1/src/main/java/config/ProjectConfig.java
+++ SpringStartCh06_1/src/main/java/config/ProjectConfig.java
@@ -1,9 +1,19 @@
 package config;
 
+import aspects.LoggingAspect;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan(basePackages = "services")
+@EnableAspectJAutoProxy
 public class ProjectConfig {
+
+    //add instance of the LoggingAspect class to spring context
+    @Bean
+    public LoggingAspect aspect(){
+        return new LoggingAspect();
+    }
 }
