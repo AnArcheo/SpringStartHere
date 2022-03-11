3694d1bfa95188cc4c3da972ef5de679671f0e26
Anna Łuczak <an.archeo@gmail.com>
2022-03-11 23:56:49 +0000

    updated LoggingAspect

--- SpringStartCh06_2/src/main/java/aspect/LoggingAspect.java
+++ SpringStartCh06_2/src/main/java/aspect/LoggingAspect.java
@@ -1,11 +1,11 @@
 package aspect;
 
+import model.Comment;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.logging.Logger;
 
 @Aspect
@@ -15,16 +15,21 @@ public class LoggingAspect {
     public Object log(ProceedingJoinPoint joinPoint) throws Throwable{
         String methodName = joinPoint.getSignature().getName();
         Object[] arguments = joinPoint.getArgs();
-
+        //System.out.println(arguments[0]);
 
         logger.info("Method " + methodName +
                 " with parameters " + Arrays.asList(arguments) +
                 " will execute");
 
-        Object returnedByMethod = joinPoint.proceed();
+        //altering parameters and returned value
+        Comment comment = new Comment();
+        comment.setText("Some other text!");
+        Object[] newArguments = {comment};
+
+        Object returnedByMethod = joinPoint.proceed(newArguments); //send different comment instance as value to the method parameter
 
         logger.info("Method executed and returned " + returnedByMethod);
 
-        return returnedByMethod;
+        return "FAILED"; //log the value returned by the intercepted method but return different value to the caller
     }
 }
