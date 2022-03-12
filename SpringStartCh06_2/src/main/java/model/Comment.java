1a37b78ecfcfcb960793c19c7bed1412da95a946
Anna Łuczak <an.archeo@gmail.com>
2022-03-12 21:35:21 +0000

    updated Comment

--- SpringStartCh06_2/src/main/java/model/Comment.java
+++ SpringStartCh06_2/src/main/java/model/Comment.java
@@ -19,4 +19,12 @@ public class Comment {
     public void setAuthor(String author) {
         this.author = author;
     }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "author='" + author + '\'' +
+                ", text='" + text + '\'' +
+                '}';
+    }
 }
