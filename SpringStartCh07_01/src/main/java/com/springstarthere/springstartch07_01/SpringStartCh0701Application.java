8e173136a06f7afde156d2ad0e395b7560f26bec
Anna Łuczak <an.archeo@gmail.com>
2022-03-13 22:11:00 +0000

    initial commit Ch07

---
+++ SpringStartCh07_01/src/main/java/com/springstarthere/springstartch07_01/SpringStartCh0701Application.java
@@ -0,0 +1,13 @@
+package com.springstarthere.springstartch07_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartCh0701Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStartCh0701Application.class, args);
+    }
+
+}
---
+++ SpringStartCh07_01/src/test/java/com/springstarthere/springstartch07_01/SpringStartCh0701ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstarthere.springstartch07_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartCh0701ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
