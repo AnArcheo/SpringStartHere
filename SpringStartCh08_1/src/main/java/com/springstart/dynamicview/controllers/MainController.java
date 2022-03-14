6a4df98be1ed9769072281dfacffe219f7bfb44c
Anna Łuczak <an.archeo@gmail.com>
2022-03-14 23:32:13 +0000

    updated MainController Ch08

--- SpringStartCh08_1/src/main/java/com/springstart/dynamicview/controllers/MainController.java
+++ SpringStartCh08_1/src/main/java/com/springstart/dynamicview/controllers/MainController.java
@@ -3,13 +3,16 @@ package com.springstart.dynamicview.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
     @RequestMapping("/home")
-    public String home(Model page){
-        page.addAttribute("username", "Katy");
-        page.addAttribute("color", "red");
+    public String home(@RequestParam(required = false) String color,
+                       @RequestParam(required = false) String name,
+                       Model page){
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
 
         return "home.html";
     }
