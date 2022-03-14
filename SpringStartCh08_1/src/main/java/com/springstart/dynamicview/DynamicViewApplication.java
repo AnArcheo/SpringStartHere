9c16004a4f0df2f1bca25a7eb8a12415d59361f0
Anna Łuczak <an.archeo@gmail.com>
2022-03-14 23:11:24 +0000

    initial commit Ch08

---
+++ SpringStartCh08_1/src/main/java/com/springstart/dynamicview/DynamicViewApplication.java
@@ -0,0 +1,13 @@
+package com.springstart.dynamicview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicViewApplication.class, args);
+    }
+
+}
