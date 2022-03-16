26a2585ed08ffda8a48c34909846a1ec8a8b8864
Anna Łuczak <an.archeo@gmail.com>
2022-03-16 23:54:29 +0000

    initial commit

---
+++ SpringStartCh08_2/src/main/java/com/example/springstartch08_2/SpringStartCh082Application.java
@@ -0,0 +1,13 @@
+package com.example.springstartch08_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartCh082Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStartCh082Application.class, args);
+    }
+
+}
---
+++ SpringStartCh08_2/src/test/java/com/example/springstartch08_2/SpringStartCh082ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springstartch08_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartCh082ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
