b0bd59a11cf1dfdab7eeb00b2141a1d3cb2ef2ec
Anna Łuczak <an.archeo@gmail.com>
2022-03-17 21:56:53 +0000

    created ProductsController

--- SpringStartCh08_2/src/main/java/com/example/springstartch08_2/controllers/ProductsController.java
+++ SpringStartCh08_2/src/main/java/com/example/springstartch08_2/controllers/ProductsController.java
@@ -2,6 +2,8 @@ package com.example.springstartch08_2.controllers;
 
 import com.example.springstartch08_2.services.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProductsController {
@@ -10,5 +12,12 @@ public class ProductsController {
 
     public ProductsController(ProductService productService) {
         this.productService = productService;
+    }
+    @RequestMapping("/products") // requestmapping by default uses http get method
+    public String viewProducts(Model model){ // model param used to send data to view
+        var products = productService.findAll(); // get the list of products from service
+        model.addAttribute("products", products); //send the product list to view
+
+        return "/products"; // return the view name that will be taken and rendered by dispatcher servlet
     }
 }
