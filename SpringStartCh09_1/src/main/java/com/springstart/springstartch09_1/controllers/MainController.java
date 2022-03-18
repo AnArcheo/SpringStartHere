fc5038aa72cf50536fa8c38404eccb78dda0efc1
Anna Łuczak <an.archeo@gmail.com>
2022-03-18 22:12:10 +0000

    update main view page and MainController

--- SpringStartCh09_1/src/main/java/com/springstart/springstartch09_1/controllers/MainController.java
+++ SpringStartCh09_1/src/main/java/com/springstart/springstartch09_1/controllers/MainController.java
@@ -2,7 +2,9 @@ package com.springstart.springstartch09_1.controllers;
 
 import com.springstart.springstartch09_1.services.LoggedUserManagementService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -14,12 +16,19 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String home(){
+    public String home(@RequestParam (required = false) String logout, Model model){
+
+        if(logout != null){
+            loggedUserManagementService.setUsername(null); // if logout param present - erase username
+        }
+
         String username = loggedUserManagementService.getUsername();
 
         if(username == null){
             return "redirect:/";
         }
+
+        model.addAttribute("username", username);
         return "/main";
     }
 }
