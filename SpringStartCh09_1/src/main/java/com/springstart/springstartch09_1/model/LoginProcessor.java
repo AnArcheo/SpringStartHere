daa8df3a322482c8675283b4b8a46860fd77ef6a
Anna Łuczak <an.archeo@gmail.com>
2022-03-18 21:12:54 +0000

    updated LoginProcessor

--- SpringStartCh09_1/src/main/java/com/springstart/springstartch09_1/model/LoginProcessor.java
+++ SpringStartCh09_1/src/main/java/com/springstart/springstartch09_1/model/LoginProcessor.java
@@ -1,11 +1,18 @@
 package com.springstart.springstartch09_1.model;
 
+import com.springstart.springstartch09_1.services.LoggedUserManagementService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
 @Component
 @RequestScope // login scope
 public class LoginProcessor {
+
+    private final LoggedUserManagementService loggedUserManagementService;
+
+    public LoginProcessor(LoggedUserManagementService loggedUserManagementService) {
+        this.loggedUserManagementService = loggedUserManagementService;
+    }
 
     private String username;
     private String password;
@@ -31,11 +38,16 @@ public class LoginProcessor {
         String username = this.getUsername();
         String password = this.getPassword();
 
+        boolean loginResult = false;
+
         if("Anna".equals(username) && "password".equals(password)){
-            return true;
+            loginResult = true;
+            loggedUserManagementService.setUsername(username); // store username on the LoggedUserManagementService bean
         }else{
-            return false;
+            loginResult = false;
         }
+
+        return loginResult;
     }
 
 
