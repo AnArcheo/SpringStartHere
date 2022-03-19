97d124bfbc7c01b9bcc8f34b16387145d2f6bdb2
Anna Łuczak <an.archeo@gmail.com>
2022-03-19 21:32:08 +0000

    rest example2 - updated CountryController

--- SpringStartCh10_2/src/main/java/com/springstart/springstartch10_2/controller/CountryController.java
+++ SpringStartCh10_2/src/main/java/com/springstart/springstartch10_2/controller/CountryController.java
@@ -4,6 +4,8 @@ import com.springstart.springstartch10_2.model.Country;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class CountryController {
 
@@ -11,5 +13,13 @@ public class CountryController {
     public Country france(){
         Country country = Country.of("France", 67);
         return country;
+    }
+
+    //return a collection
+    @GetMapping("/all")
+    public List<Country> countries(){
+        Country c1 = Country.of("France", 67);
+        Country c2 = Country.of("Spain", 47);
+        return List.of(c1, c2); //returns a collection in the HTTP response body
     }
 }
