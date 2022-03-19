9c9511aa8677af4a6bb39e33f9771516efa2dd55
Anna Łuczak <an.archeo@gmail.com>
2022-03-19 21:42:16 +0000

    rest example2 - updated CountryController

--- SpringStartCh10_2/src/main/java/com/springstart/springstartch10_2/controller/CountryController.java
+++ SpringStartCh10_2/src/main/java/com/springstart/springstartch10_2/controller/CountryController.java
@@ -1,6 +1,8 @@
 package com.springstart.springstartch10_2.controller;
 
 import com.springstart.springstartch10_2.model.Country;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,16 @@ public class CountryController {
         Country c1 = Country.of("France", 67);
         Country c2 = Country.of("Spain", 47);
         return List.of(c1, c2); //returns a collection in the HTTP response body
+    }
+
+    //added custom header and setting response status
+    @GetMapping("/germany")
+    public ResponseEntity<Country> germany(){
+        Country c = Country.of("Germany", 60);
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .header("continent", "Europe")
+                .header("capital", "Berlin")
+                .header("favourite_food", "beer")
+                .body(c);
     }
 }
