95297f937a6f5551aa4eb24c056c9e12b5313376
Anna Łuczak <an.archeo@gmail.com>
2022-03-19 22:50:46 +0000

    rest example3 - added PaymentDetails and Error classes

---
+++ SpringStartCh10_3/src/main/java/com/springstart/springstartch10_3/model/ErrorDetails.java
@@ -0,0 +1,14 @@
+package com.springstart.springstartch10_3.model;
+
+public class ErrorDetails {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ SpringStartCh10_3/src/main/java/com/springstart/springstartch10_3/model/PaymentDetails.java
@@ -0,0 +1,14 @@
+package com.springstart.springstartch10_3.model;
+
+public class PaymentDetails {
+
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
