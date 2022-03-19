795b36f37dca377f9d9ce35f5526d6c8be64203e
Anna Łuczak <an.archeo@gmail.com>
2022-03-19 21:06:53 +0000

    rest example2

---
+++ SpringStartCh10_2/src/main/java/com/springstart/springstartch10_2/SpringStartCh102Application.java
@@ -0,0 +1,13 @@
+package com.springstart.springstartch10_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartCh102Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStartCh102Application.class, args);
+    }
+
+}
---
+++ SpringStartCh10_2/src/test/java/com/springstart/springstartch10_2/SpringStartCh102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstart.springstartch10_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartCh102ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
