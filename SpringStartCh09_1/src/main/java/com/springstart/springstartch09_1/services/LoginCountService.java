f7ca972f9b6df9653d609b2c6cad56f485afef7b
Anna Łuczak <an.archeo@gmail.com>
2022-03-19 00:38:08 +0000

    added LoginCountService with application scope

---
+++ SpringStartCh09_1/src/main/java/com/springstart/springstartch09_1/services/LoginCountService.java
@@ -0,0 +1,19 @@
+package com.springstart.springstartch09_1.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+@Service
+@ApplicationScope
+public class LoginCountService {
+
+    private int count;
+
+    public void increment(){
+        count++;
+    }
+
+    public int getCount(){
+        return count;
+    }
+}
