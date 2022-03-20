b4089095dbfb52b62d1c8cb9fb45e33054fcca51
Anna Łuczak <an.archeo@gmail.com>
2022-03-20 13:56:17 +0000

    rest example4 - initial commit

---
+++ SpringStartCh11_1/src/main/java/com/springstart/springstartch11_1/SpringStartCh111Application.java
@@ -0,0 +1,13 @@
+package com.springstart.springstartch11_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartCh111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStartCh111Application.class, args);
+    }
+
+}
