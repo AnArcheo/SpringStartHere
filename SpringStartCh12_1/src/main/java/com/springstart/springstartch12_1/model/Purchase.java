d33f5e8bc38a7868aa269c9d192994986b71d0cb
Anna Łuczak <an.archeo@gmail.com>
2022-03-23 23:15:45 +0000

    created Purchase model

---
+++ SpringStartCh12_1/src/main/java/com/springstart/springstartch12_1/model/Purchase.java
@@ -0,0 +1,33 @@
+package com.springstart.springstartch12_1.model;
+
+import java.math.BigDecimal;
+
+public class Purchase {
+    private int id;
+    private String product;
+    private BigDecimal price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
