c4fb2d25cb39ea32aab0ff0d4474db5ba026f1a5
Anna Łuczak <an.archeo@gmail.com>
2022-03-23 23:07:54 +0000

    Ch 12 - initial commit

---
+++ SpringStartCh12_1/src/main/java/com/springstart/springstartch12_1/SpringStartCh121Application.java
@@ -0,0 +1,13 @@
+package com.springstart.springstartch12_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartCh121Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStartCh121Application.class, args);
+    }
+
+}
---
+++ SpringStartCh12_1/src/test/java/com/springstart/springstartch12_1/SpringStartCh121ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstart.springstartch12_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartCh121ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
