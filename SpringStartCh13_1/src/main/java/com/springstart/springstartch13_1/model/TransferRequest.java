759fcc0cdc2a95a2a9ebb84460e66c98b327d6dc
Anna Łuczak <an.archeo@gmail.com>
2022-03-26 23:39:25 +0000

    Chapter 13 - created TransferRequest

---
+++ SpringStartCh13_1/src/main/java/com/springstart/springstartch13_1/model/TransferRequest.java
@@ -0,0 +1,34 @@
+package com.springstart.springstartch13_1.model;
+
+import java.math.BigDecimal;
+
+public class TransferRequest {
+
+    private long senderAccountId;
+    private long receiverAccountId;
+    private BigDecimal amount;
+
+    public long getSenderAccountId() {
+        return senderAccountId;
+    }
+
+    public void setSenderAccountId(long senderAccountId) {
+        this.senderAccountId = senderAccountId;
+    }
+
+    public long getReceiverAccountId() {
+        return receiverAccountId;
+    }
+
+    public void setReceiverAccountId(long receiverAccountId) {
+        this.receiverAccountId = receiverAccountId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
