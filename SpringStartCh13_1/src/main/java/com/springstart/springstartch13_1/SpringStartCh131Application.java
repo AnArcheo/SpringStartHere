d8b8634ad3fc4837f128dab44cc30717ecf1ff1e
Anna Łuczak <an.archeo@gmail.com>
2022-03-26 22:19:42 +0000

    Chapter 13 - initial commit

---
+++ SpringStartCh13_1/src/main/java/com/springstart/springstartch13_1/SpringStartCh131Application.java
@@ -0,0 +1,13 @@
+package com.springstart.springstartch13_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartCh131Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStartCh131Application.class, args);
+    }
+
+}
