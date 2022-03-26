0fc4fbb141863071923f56316fcdb630dd8ad05f
Anna Łuczak <an.archeo@gmail.com>
2022-03-26 22:54:04 +0000

    Chapter 13 - created Account model

---
+++ SpringStartCh13_1/src/main/java/com/springstart/springstartch13_1/model/Account.java
@@ -0,0 +1,33 @@
+package com.springstart.springstartch13_1.model;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private Long id;
+    private String name;
+    private BigDecimal amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
