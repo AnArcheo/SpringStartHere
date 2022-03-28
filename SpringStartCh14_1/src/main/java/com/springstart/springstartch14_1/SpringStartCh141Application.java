1e2d2d410e62ade82e7d41d13c4d239e2f34cf0f
Anna Łuczak <an.archeo@gmail.com>
2022-03-28 23:31:05 +0100

    Chapter 14 - initial commit

---
+++ SpringStartCh14_1/src/main/java/com/springstart/springstartch14_1/SpringStartCh141Application.java
@@ -0,0 +1,13 @@
+package com.springstart.springstartch14_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartCh141Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStartCh141Application.class, args);
+    }
+
+}
---
+++ SpringStartCh14_1/src/test/java/com/springstart/springstartch14_1/SpringStartCh141ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstart.springstartch14_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStartCh141ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
