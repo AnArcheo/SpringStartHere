74f6858647ba353431118e4683bf88998438cf1e
Anna Łuczak <an.archeo@gmail.com>
2022-03-29 23:50:14 +0100

    Chapter 14 - updated AccountRepository

--- SpringStartCh14_1/src/main/java/com/springstart/springstartch14_1/repositories/AccountRepository.java
+++ SpringStartCh14_1/src/main/java/com/springstart/springstartch14_1/repositories/AccountRepository.java
@@ -1,7 +1,20 @@
 package com.springstart.springstartch14_1.repositories;
 
 import com.springstart.springstartch14_1.model.Account;
+
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface AccountRepository extends CrudRepository<Account, Long> {
+
+    @Query("SELECT * FROM account WHERE name = :name")
+    List<Account> findAccountsByName(String name);
+
+    @Modifying
+    @Query("UPDATE account SET amount = :amount WHERE id = :id")
+    void changeAmount(Long id, BigDecimal amount);
 }
