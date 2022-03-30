c5e8870c952092382d38e5d6878cc1b5d92b8f3e
Anna Łuczak <an.archeo@gmail.com>
2022-03-30 23:19:16 +0100

    Chapter 14 - updated TransferService

--- SpringStartCh14_1/src/main/java/com/springstart/springstartch14_1/services/TransferService.java
+++ SpringStartCh14_1/src/main/java/com/springstart/springstartch14_1/services/TransferService.java
@@ -4,8 +4,10 @@ import com.springstart.springstartch14_1.exceptions.AccountNotFoundException;
 import com.springstart.springstartch14_1.model.Account;
 import com.springstart.springstartch14_1.repositories.AccountRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class TransferService {
@@ -15,7 +17,7 @@ public class TransferService {
     public TransferService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
     }
-
+    @Transactional
     public void transferMoney(Long idSender, Long idReceiver, BigDecimal amount){
         Account sender = accountRepository.findById(idSender)
             .orElseThrow(()-> new AccountNotFoundException());
@@ -30,4 +32,13 @@ public class TransferService {
         accountRepository.changeAmount(idReceiver, receiverNewAmount);
 
     }
+
+    public Iterable<Account> getAllAccounts(){
+        return accountRepository.findAll();
+    }
+
+    public List<Account> findAccountsByName(String name){
+        return accountRepository.findAccountsByName(name);
+    }
+
 }
