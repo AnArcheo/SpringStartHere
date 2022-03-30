01002d62b5f1ab87c2421c01a45a18490fce4b6f
Anna Łuczak <an.archeo@gmail.com>
2022-03-30 23:30:38 +0100

    Chapter 14 - created AccountController

---
+++ SpringStartCh14_1/src/main/java/com/springstart/springstartch14_1/controllers/AccountController.java
@@ -0,0 +1,32 @@
+package com.springstart.springstartch14_1.controllers;
+
+import com.springstart.springstartch14_1.dto.TransferRequest;
+import com.springstart.springstartch14_1.model.Account;
+import com.springstart.springstartch14_1.services.TransferService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AccountController {
+
+    private final TransferService transferService;
+
+    public AccountController(TransferService transferService) {
+        this.transferService = transferService;
+    }
+
+    @PostMapping("/transfer")
+    public void transferMoney(@RequestBody TransferRequest transferRequest){
+        transferService.transferMoney(transferRequest.getSenderAccountId(),
+                                      transferRequest.getReceiverAccountId(),
+                                      transferRequest.getAmount());
+    }
+
+    @GetMapping("/accounts")
+    public Iterable<Account> getAllAccounts(@RequestParam(required = false) String name){
+        if(name == null){
+            return transferService.getAllAccounts();
+        }else{
+            return transferService.findAccountsByName(name);
+        }
+    }
+}
