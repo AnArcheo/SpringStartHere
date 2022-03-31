45c5dca96ddedfeb1823ea530b590639c8d77ff1
Anna Łuczak <an.archeo@gmail.com>
2022-03-31 23:28:05 +0100

    Chapter 14 - updated schema.sql

--- SpringStartCh14_1/src/main/resources/schema.sql
+++ SpringStartCh14_1/src/main/resources/schema.sql
@@ -1,5 +1,6 @@
-create table account{
-    id NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR (50) NOT NULL,
-    amount DOUBLE NOT NULL
-    };
+create table account
+(
+    id     INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name   VARCHAR(50) NOT NULL,
+    amount DOUBLE      NOT NULL
+);
