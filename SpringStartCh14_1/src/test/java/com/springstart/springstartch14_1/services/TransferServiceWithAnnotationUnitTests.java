77d00b09654750d13fcc03880f0c6415dd7fb2c9
Anna Łuczak <an.archeo@gmail.com>
2022-04-09 23:43:58 +0100

    Chapter 14 - Test TransferService

--- SpringStartCh14_1/src/test/java/com/springstart/springstartch14_1/services/TransferServiceWithAnnotationUnitTests.java
+++ SpringStartCh14_1/src/test/java/com/springstart/springstartch14_1/services/TransferServiceWithAnnotationUnitTests.java
@@ -1,5 +1,6 @@
 package com.springstart.springstartch14_1.services;
 
+import com.springstart.springstartch14_1.exceptions.AccountNotFoundException;
 import com.springstart.springstartch14_1.model.Account;
 import com.springstart.springstartch14_1.repositories.AccountRepository;
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.math.BigDecimal;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -43,5 +48,24 @@ public class TransferServiceWithAnnotationUnitTests {
         //verify that changeAmount() method was called with expected parameters
         verify(accountRepository).changeAmount(1L, new BigDecimal(900));
         verify(accountRepository).changeAmount(2L, new BigDecimal(1100));
+    }
+
+    @Test
+    public void moneyTransferDestinationAccountNotFoundFlow(){
+        Account sender = new Account();
+        sender.setId(1L);
+        sender.setAmount(new BigDecimal(1000));
+
+        given(accountRepository.findById(1L))
+                .willReturn(Optional.of(sender));
+
+        //to return empty Optional when findById() is called
+        given(accountRepository.findById(2L))
+                .willReturn(Optional.empty());
+
+        assertThrows(AccountNotFoundException.class,
+                ()->transferService.transferMoney(1L,2L,new BigDecimal(100L)));
+
+        verify(accountRepository, never()).changeAmount(anyLong(), any());
     }
 }
